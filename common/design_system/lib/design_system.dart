ec5a8fa14857b5db8fff7158e4d89adebb7cb555
Manoel Soares Neto <msrsneto@gmail.com>
2024-07-29 12:27:33 -0300

    Create single layout scroll builder

--- common/design_system/lib/design_system.dart
+++ common/design_system/lib/design_system.dart
@@ -3,3 +3,4 @@ library design_system;
 export 'src/theme/app_theme.dart';
 export 'src/utils/spacings.dart';
 export 'src/widgets/search_bar.dart';
+export 'src/widgets/single_layout_scroll_builder.dart';
